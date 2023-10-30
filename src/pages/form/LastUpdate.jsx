49aee2f813f2020db60b503bab89d3c4e3ba2deb
tyndareu <flmatamoros@gmail.com>
2023-10-30 08:14:43 +0100

    changed order of last update

--- src/pages/form/LastUpdate.jsx
+++ src/pages/form/LastUpdate.jsx
@@ -16,9 +16,9 @@ export default function LastUpdate({ lastUpdate }) {
           <ol className="mt-3 list-disc">
             {lastUpdate.map((update, index) => (
               <li key={index} className="mb-1  text-gray-400">
-                <span className="text-orange-500"> {update.split('-')[0]}</span>
+                <span className="text-orange-500"> {update.split('-')[1]}</span>
                 :
-                <span className="text-yellow-500">{update.split('-')[1]} </span>
+                <span className="text-yellow-500">{update.split('-')[0]} </span>
                 -<span className="text-blue-500">{update.split('-')[2]}</span>
               </li>
             ))}
