478d3f3276be7bcecc84087ab3945fb50bc3c4a6
Francisco Matamoros <francisco.matamorosmoreno@plexus.es>
2024-02-06 09:17:36 +0100

    updated dependencies and updated statistics styles

--- src/pages/statistics/Statistics.jsx
+++ src/pages/statistics/Statistics.jsx
@@ -77,10 +77,7 @@ export default function Statistics() {
 
   return (
     <>
-      <h1 className="text-center text-3xl font-bold text-blue-500">
-        Estadísticas
-      </h1>
-      <div className="mb-4 mt-3 flex flex-wrap justify-center gap-1">
+      <div className="m-2 flex flex-wrap justify-center gap-1">
         {offersByYears &&
           Object.keys(offersByYears).map((year) => (
             <button
@@ -89,8 +86,8 @@ export default function Statistics() {
               className={`rounded p-1 px-2 transition-colors duration-300
                   ${
                     year === selectedYear
-                      ? 'bg-blue-500 text-gray-200 hover:bg-blue-700'
-                      : 'border border-blue-500 text-blue-500 hover:bg-blue-700 hover:text-gray-200'
+                      ? 'bg-orange-500 font-bold text-gray-900 hover:bg-orange-700'
+                      : 'border border-orange-500 font-bold text-orange-500 hover:bg-orange-700 hover:text-gray-900'
                   }`}
             >
               {year}
@@ -98,33 +95,55 @@ export default function Statistics() {
           ))}
       </div>
 
-      <div className="flex flex-wrap justify-center gap-1">
+      <div className="flex flex-wrap justify-center gap-2">
         {[selectedMonthFirst, selectedMonthSecond].map(
           (selectedMonth, index) => (
             <div
               key={index}
               className="w-[48%] min-w-[650px] rounded-xl border border-blue-500 p-1"
             >
-              <div className="m-3 flex flex-wrap justify-center gap-2">
+              <div className="m-2 flex justify-center">
+                {monthsFromYears && (
+                  <button
+                    onClick={() =>
+                      index === 0
+                        ? setSelectedMonthFirst(monthsFromYears[0])
+                        : setSelectedMonthSecond(monthsFromYears[0])
+                    }
+                    className={`rounded p-1 px-2 transition-colors duration-300
+                  ${
+                    monthsFromYears[0] === selectedMonth
+                      ? 'border border-green-900 bg-green-500 font-bold text-gray-900 hover:border-gray-500'
+                      : 'border border-green-500 font-bold text-green-500 hover:bg-green-700 hover:text-gray-200'
+                  }`}
+                  >
+                    {months[monthsFromYears[0]]}
+                  </button>
+                )}
+              </div>
+              <div className="m-2 flex flex-wrap justify-center gap-2">
                 {monthsFromYears &&
-                  monthsFromYears.map((month) => (
-                    <button
-                      onClick={() =>
-                        index === 0
-                          ? setSelectedMonthFirst(month)
-                          : setSelectedMonthSecond(month)
-                      }
-                      className={`rounded p-1 px-2 transition-colors duration-300
+                  monthsFromYears.map(
+                    (month) =>
+                      month !== '00' && (
+                        <button
+                          onClick={() =>
+                            index === 0
+                              ? setSelectedMonthFirst(month)
+                              : setSelectedMonthSecond(month)
+                          }
+                          className={`rounded p-1 px-2 transition-colors duration-300
                   ${
                     month === selectedMonth
                       ? 'bg-blue-500 text-gray-200 hover:bg-blue-700'
                       : 'border border-blue-500 text-blue-500 hover:bg-blue-700 hover:text-gray-200'
                   }`}
-                      key={month}
-                    >
-                      {months[month]}
-                    </button>
-                  ))}
+                          key={month}
+                        >
+                          {months[month]}
+                        </button>
+                      )
+                  )}
               </div>
               <h2 className="text-center text-blue-500">
                 Rendimiento de Ofertas y Control de Finalización
