64b12f32147fdd580f544a26bad5c6538d3cbdf2
FlMatamoros <tyndareus1274@gmail.com>
2024-02-19 11:05:54 +0100

    changed OL to Table on LastUpdate

--- src/pages/form/LastUpdate.jsx
+++ src/pages/form/LastUpdate.jsx
@@ -14,16 +14,24 @@ export default function LastUpdate({ lastUpdate }) {
           {showUpdates ? 'Ocultar Cambios' : 'Mostrar Cambios'}
         </button>
         {showUpdates && (
-          <ol className="mt-3 list-disc">
-            {lastUpdate.map((update, index) => (
-              <li key={index} className="mb-1  text-gray-400">
-                <span className="text-orange-500"> {update.split('-')[1]}</span>
-                :
-                <span className="text-yellow-500">{update.split('-')[0]} </span>
-                -<span className="text-blue-500">{update.split('-')[2]}</span>
-              </li>
-            ))}
-          </ol>
+          <table className="mt-2">
+            <thead>
+              <tr className='text-blue-500'>
+                <th>Fecha</th>
+                <th>Acción</th>
+                <th>Usuario</th>
+              </tr>
+            </thead>
+            <tbody>
+              {lastUpdate.map((update, index) => (
+                <tr key={index}>
+                  <td className="px-5 text-orange-500">{update.split('-')[1]}</td>
+                  <td className="px-5 text-yellow-500">{update.split('-')[0]}</td>
+                  <td className='text-blue-500'>{update.split('-')[2]}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         )}
       </>
     )
